927495e742b3ae58abc326f2abac0a3f4378fc60
marekbielsky <marekbielski@yahoo.com>
2024-11-18 18:40:19 +0100

    allow to fetch data if invalid symbol

--- src/binance/dtos/binance.dto.ts
+++ src/binance/dtos/binance.dto.ts
@@ -1,9 +1,9 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsEnum, IsNumber, IsOptional } from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 import { BinanceSymbol } from '../services/types/binance.types';
 
 export class BinanceExerciseHistoryDTO {
-  @IsEnum(BinanceSymbol)
+  @IsString()
   @ApiProperty({
     example: 'BTC-241118-89500-C',
     description: "it's a Binance API symbol",
