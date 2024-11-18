62d03c1ae7de00989016e734ae54a99233eb1141
marekbielsky <marekbielski@yahoo.com>
2024-11-18 19:42:10 +0100

    introduce limit value for the query dto

--- src/binance/dtos/binance.dto.ts
+++ src/binance/dtos/binance.dto.ts
@@ -1,11 +1,20 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsNumber, IsOptional, IsString } from 'class-validator';
+import {
+  IsMilitaryTime,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
 import { BinanceSymbol } from '../services/types/binance.types';
+import { now, yesterday } from '../../common/utils/date.util';
 
 export class BinanceExerciseHistoryDTO {
   @IsString()
   @ApiProperty({
-    example: 'BTCUSDT',
+    example: BinanceSymbol.BTCUSDT,
     description:
       "it's a Binance API symbol responsible for fetching correct data",
   })
@@ -13,17 +22,34 @@ export class BinanceExerciseHistoryDTO {
 
   @IsOptional()
   @IsNumber()
+  @IsPositive()
   @ApiPropertyOptional({
     example: new Date().getTime(),
-    description: 'time in ms must be provided',
+    description: 'time in ms',
+    default: yesterday(),
   })
-  public startTime?: number;
+  public startTime?: number = yesterday();
 
   @IsOptional()
   @IsNumber()
+  @IsPositive()
   @ApiPropertyOptional({
     example: new Date().getTime(),
-    description: 'time in ms must be provided',
+    description: 'time in ms',
+    default: now(),
   })
-  public endTime?: number;
+  public endTime?: number = now();
+
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  @Max(100)
+  @ApiPropertyOptional({
+    example: 25,
+    description: 'limit of fetched records',
+    default: 100,
+    minimum: 1,
+    maximum: 100,
+  })
+  public limit?: number = 100;
 }
--- src/binance/services/binance.service.ts
+++ src/binance/services/binance.service.ts
@@ -1,19 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { BinanceExerciseHistoryDTO } from '../dtos/binance.dto';
 import { BinanceApiRoutes } from '../../common/routes/routes.enum';
-import { getExerciseHistoryParameters } from './utils/binance.utils';
 import { ExerciseHistoryDataRes } from '../responses/binance.response';
 
 @Injectable()
 export class BinanceService {
-  public async getExerciseHistory(
-    query: BinanceExerciseHistoryDTO,
-  ): Promise<ExerciseHistoryDataRes[]> {
-    const { underlying, startTime, endTime } =
-      getExerciseHistoryParameters(query);
-
+  public async getExerciseHistory({
+    underlying,
+    startTime,
+    endTime,
+    limit,
+  }: BinanceExerciseHistoryDTO): Promise<ExerciseHistoryDataRes[]> {
     const res = await fetch(
-      `${BinanceApiRoutes.ExerciseHistory}?underlying=${underlying}&startTime=${startTime}&endTime=${endTime}`,
+      `${BinanceApiRoutes.ExerciseHistory}?underlying=${underlying}&startTime=${startTime}&endTime=${endTime}&limit=${limit}`,
     );
 
     return res.json();
