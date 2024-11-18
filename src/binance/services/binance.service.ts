aa3bef2d0c1394f58839b8b426546fed284c932b
marekbielsky <marekbielski@yahoo.com>
2024-11-18 16:09:53 +0100

    add date parameters to fetch query

--- src/binance/services/binance.service.ts
+++ src/binance/services/binance.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { subDays } from 'date-fns';
 
 @Injectable()
 export class BinanceService {
   public async getHistoricalMarketData(
     underlying: string = 'BTCUSDT',
+    startTime: number = subDays(new Date(), 1).getTime(),
+    endTime: number = new Date().getTime(),
   ): Promise<void> {
     const res = await fetch(
-      `https://eapi.binance.com/eapi/v1/exerciseHistory?underlying=${underlying}`,
+      `https://eapi.binance.com/eapi/v1/exerciseHistory?underlying=${underlying}&startTime=${startTime}&endTime=${endTime}`,
     );
     const data = await res.json();
 
-    console.log(data.length);
+    console.log(data);
   }
 }
